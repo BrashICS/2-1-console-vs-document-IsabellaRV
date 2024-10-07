25359478c0c47576a7c72837046b3f5d01d5ed9a
IsabellaRV <isabella.ramos-valeriano@stu.ocsb.ca>
2024-10-07 10:35:07 -0400

    oops

--- main.js
+++ main.js
@@ -7,4 +7,10 @@
 
 
 // Feel free to delete this line when starting the task
-console.log("Hello world!")
+console.log("Hello\tworld!")
+console.log("So\tCool")
+console.log("Apple Inc\nOne Apple Park Way\nCupertino, CA\n95014\n+1 408-996-1010")
+console.log("\n\n")
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080")
+console.log("\n\n")
+console.log("x\to\tx\no\tx\to\nx\to\tx")
